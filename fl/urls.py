7b449cb316860ef917fcef4b7fed654b5c31835d
sudhakar6295 <53999168+sudhakar6295@users.noreply.github.com>
2024-05-16 20:25:26 +0530

    adding pagination

--- fl/urls.py
+++ fl/urls.py
@@ -6,5 +6,6 @@ urlpatterns = [
     path('search/', views.search, name='search'),
     path('<str:category>/', views.category_view, name='category_view'),
     path('<str:category>/<str:sku>/', views.product_detail, name='product_detail'),
+    path('<str:category>/page/<int:page>/', views.category_view, name='category_view')
     
 ]
--- fl/views.py
+++ fl/views.py
@@ -1,14 +1,22 @@
 from django.shortcuts import render
 from fl.extraction.data_extraction import fetch_categories,fetch_products,fetch_product_detail,search_product
-
+from django.core.paginator import Paginator, PageNotAnInteger, EmptyPage
 # Create your views here.
 def index(request):
     categorys = fetch_categories()
     return render(request, 'index.html',{'categorys':categorys})
 
-def category_view(request, category):
+def category_view(request, category,page=1):
     products = fetch_products(category)
-    return render(request, 'category.html',{'products':products})
+    page = request.GET.get('page', 1)
+    paginator = Paginator(products, 20)
+    try:
+        items = paginator.page(page)
+    except PageNotAnInteger:
+        items = paginator.page(1)
+    except EmptyPage:
+        items = paginator.page(paginator.num_pages)
+    return render(request, 'category.html',{'products':items,'page':items})
 
 
 def product_detail(request, category,sku):
