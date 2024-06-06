3b7a7d63fcad73b08ff416f053208c5d066d6ebe
sudhakar6295 <53999168+sudhakar6295@users.noreply.github.com>
2024-06-06 21:42:16 +0530

    Update settings.py

--- fold_fronend/settings.py
+++ fold_fronend/settings.py
@@ -78,9 +78,9 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',  # Change this to 'django.db.backends.postgresql', 'django.db.backends.sqlite3', or 'django.db.backends.oracle' depending on your database
         'NAME': 'fold',  # Replace 'fold' with your database name
-        'USER': 'root',  # Replace 'fold' with your database username
-        'PASSWORD': 'fold',  # Replace 'XLqV6yPnwklZvNVL' with your database password
-        'HOST': 'XLqV6yPnwklZvNVL', #'170.239.84.29',  # Replace '170.239.84.29' with your database host
+        'USER': 'fold',  # Replace 'fold' with your database username
+        'PASSWORD': 'XLqV6yPnwklZvNVL',  # Replace 'XLqV6yPnwklZvNVL' with your database password
+        'HOST': 'local', #'170.239.84.29',  # Replace '170.239.84.29' with your database host
         'PORT': '22222',  # Replace '22222' with your database port
     }
 }
